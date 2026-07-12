cd7e3d41d2387d78734cb7c92530a71141b598d2
sahoosohan <sohanxsahoo@gmail.com>
2026-07-12 18:38:18 +0530

    database setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       Hello
+      <Button>Click</Button>
     </div>
   );
 }
